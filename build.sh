48acc5b9e218cacb632cb71a789178c8a7e318bf
Kristian Zarębski <krizar312@gmail.com>
2022-02-06 18:59:32 +0000

    Updated icon

--- build.sh
+++ build.sh
@@ -1,1 +1,2 @@
-pyinstaller -F --noconsole --icon='img/ROSLauncher.ico' --clean ros_launcher.py
+SITE_PACKAGES=$(python -c 'import sysconfig; print(sysconfig.get_paths()["purelib"])')
+pyinstaller -F --noconsole --paths=$SITE_PACKAGES --icon='img/ROSLauncher.ico' --clean ros_launcher.py
