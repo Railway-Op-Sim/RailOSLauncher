d8937933c55c523ff7994ad07a4414e0ef1c605e
Kristian Zarębski <krizar312@gmail.com>
2023-03-07 23:19:06 +0000

    Added debug log writing and more exception handling

--- railos_launcher.py
+++ railos_launcher.py
@@ -6,6 +6,9 @@ This launcher provides live information on the user's current Railway Operation 
 session, giving status updates such as which route is being operated and how long for.
 As well as the mode (e.g. editting a timetable etc).
 """
+
+import pathlib
+
 __author__ = "Kristian Zarebski <krizar312@gmail.com>"
 __date__ = "2022-02-08"
 __license__ = "GPLv3"
@@ -14,7 +17,6 @@ import asyncio
 import typing
 import configparser
 import datetime
-import glob
 import logging
 import os.path
 
@@ -62,7 +64,7 @@ class DiscordBroadcaster:
 =============================================================================
 """
 
-    def __init__(self, railos_location: str, debug: bool=False) -> None:
+    def __init__(self, railos_location: str, debug: bool = False) -> None:
         if not os.path.exists(railos_location):
             raise FileNotFoundError(
                 f"Cannot location Railway Operation Simulator, path '{railos_location}' "
@@ -74,6 +76,7 @@ class DiscordBroadcaster:
         self._running = True
         self._mode = {"main": "", "oper": ""}
         self._railos_loc = railos_location
+        self._session_ini_file = os.path.join(self._railos_loc, "session.ini")
         self._discord = discordsdk.Discord(
             int(self._app_id), discordsdk.CreateFlags.default
         )
@@ -81,9 +84,34 @@ class DiscordBroadcaster:
             self.on_curr_user_update
         )
         self._activity.assets.large_image = self._logo_key
-        self._activity.assets.large_text = "Testing"
+        self._activity.assets.large_text = ""
+        self._activity.name = ""
         self._activity.details = ""
         self._activity.state = ""
+
+    def dump_log(self, method: str, exception_info: str) -> None:
+        _contents: str = pathlib.Path(self._session_ini_file).read_text()
+        _out_file: str = os.path.join(
+            os.path.dirname(__file__),
+            datetime.datetime.now().strftime("session_info_%d_%m_%y_%H_%M_%S.log"),
+        )
+
+        with open(_out_file, "w") as out_f:
+            out_f.write(
+                "\n".join([
+                    "[META]",
+                    f"RailOSLauncher Version: {self._version}",
+                    f"Mode: {self._mode.get('main', 'Unknown')}",
+                    f"Operation Mode: {self._mode.get('oper', 'Unknown')}",
+                    "[EXCEPTION]",
+                    f"Function: {method}",
+                    f"Message: {exception_info}",
+                    "[SESSION_INI_CONTENTS]",
+                    _contents
+                ])
+            )
+
+        self._logger.warning(f"Wrote error information to '{_out_file}'")
 
     def on_curr_user_update(self) -> None:
         user = self._discord.get_user_manager().get_current_user()
@@ -103,15 +131,15 @@ class DiscordBroadcaster:
             self._discord.run_callbacks()
 
     def _load_session_ini(self) -> configparser.ConfigParser:
-        _session_data = os.path.join(self._railos_loc, "session.ini")
-
-        if not os.path.exists(_session_data):
+        if not os.path.exists(self._session_ini_file):
             raise FileNotFoundError(
-                f"Expected session metadata file '{_session_data}', but file does not exist"
+                f"Expected session metadata file '{self._session_ini_file}', but file does not exist"
             )
 
         _parser = configparser.ConfigParser()
-        _parser.read(_session_data)
+
+        if not _parser.read(self._session_ini_file):
+            raise configparser.ParsingError
 
         return _parser
 
@@ -120,23 +148,35 @@ class DiscordBroadcaster:
             _metadata_file: str = parser.get("session", "metadata_file")
             return toml.load(_metadata_file)
         except configparser.NoOptionError:
-            self._logger.warning("Failed to retrieve session information from INI file,"
-                                 "this version of RailOSLauncher requires a RailOS version with API v1.2")
+            self._logger.warning(
+                "Failed to retrieve metadata, no Metadata file was specified in INI file."
+            )
             return {}
         except configparser.NoSectionError:
-            self._logger.warning("Failed to retrieve metadata file information from INI file,"
-                                 "this version of RailOSLauncher requires a RailOS version with API v1.2")
+            self._logger.warning(
+                "Failed to retrieve metadata, no Metadata file was specified in INI file."
+            )
             return {}
 
     def _update_status(self, parser: configparser.ConfigParser):
         try:
             _top_mode = Level1Mode(parser.getint("session", "main_mode"))
             _oper_mode = Level2OperMode(parser.getint("session", "operation_mode"))
-        except configparser.NoOptionError:
+        except configparser.NoOptionError as e:
             self._logger.warning("Failed to retrieve session information from INI file")
+            self.dump_log("_update_status:mode", e)
             return
-        except configparser.NoSectionError:
-            self._logger.warning("Failed to retrieve current mode information from INI file")
+        except configparser.NoSectionError as e:
+            self._logger.warning(
+                "Failed to retrieve current mode information from INI file"
+            )
+            self.dump_log("_update_status:mode", e)
+            return
+        except AttributeError as e:
+            self._logger.error(
+                f"Retrieval of session information failed with '{e}'"
+            )
+            self.dump_log("_update_status:mode", e)
             return
 
         if self._mode["main"] == _top_mode and self._mode["oper"] == _oper_mode:
@@ -155,10 +195,9 @@ class DiscordBroadcaster:
             )
         else:
             if _top_mode == Level1Mode.TrackMode:
-                _activity = "Editing"
-            else:
-                _activity = ""
-                _new_status = ""
+                _activity = "Editing "
+            elif _top_mode == Level1Mode.TimetableMode:
+                _activity = "Timetabling "
             self._activity.timestamps.start = 0
 
         if _activity:
@@ -184,12 +223,20 @@ class DiscordBroadcaster:
                             "No country found for simulation, no sub-icon will be used"
                         )
                 _new_status = f"{_activity} {_current_rly}"
-            except configparser.NoOptionError:
+            except configparser.NoOptionError as e:
                 self._logger.error("Failed to find key 'railway' in INI file")
-            except configparser.NoSectionError:
+                self.dump_log("_update_status:railway", e)
+                return
+            except configparser.NoSectionError as e:
                 self._logger.error("Failed to find section 'session' in INI file")
+                self.dump_log("_update_status:railway", e)
+                return
             except AttributeError as e:
-                self._logger.error(f"Retrieval of session information failed with '{e}'")
+                self._logger.error(
+                    f"Retrieval of session information failed with '{e}'"
+                )
+                self.dump_log("_update_status:railway", e)
+                return
 
         if self._activity.details != _new_status and _new_status:
             self._activity.details = _new_status
@@ -200,16 +247,30 @@ class DiscordBroadcaster:
     async def _check_for_temp(self) -> None:
         while self._running:
             await asyncio.sleep(2)
-            _parser = self._load_session_ini()
+            try:
+                _parser = self._load_session_ini()
+            except configparser.ParsingError:
+                self._logger.warning(
+                    "Failed to parse contents of session data, got empty list."
+                )
+                continue
             try:
                 self._running = _parser.getboolean("session", "running")
             except configparser.NoOptionError:
                 self._logger.error("Failed to find key 'running' in INI file")
+                self.dump_log("_check_for_temp", e)
+                continue
             except configparser.NoSectionError:
                 self._logger.error("Failed to find section 'session' in INI file")
+                self.dump_log("_check_for_temp", e)
+                continue
             except AttributeError as e:
                 self._logger.error(f"Retrieval of 'running' state failed with '{e}'")
-            self._update_status(_parser)
+                self.dump_log("_check_for_temp", e)
+                continue
+
+            if self._running:
+                self._update_status(_parser)
 
     async def _run_railos(self) -> None:
         if not os.path.exists(os.path.join(self._railos_loc, "railway.exe")):
@@ -221,7 +282,9 @@ class DiscordBroadcaster:
         )
 
     async def _main(self):
-        await asyncio.gather(self._run_sdk(), self._check_for_temp(), self._run_railos())
+        await asyncio.gather(
+            self._run_sdk(), self._check_for_temp(), self._run_railos()
+        )
 
     def run(self):
         self._start = datetime.datetime.now()
@@ -233,11 +296,20 @@ class DiscordBroadcaster:
 
 if __name__ in "__main__":
     import argparse
+
     _parser = argparse.ArgumentParser()
-    _parser.add_argument("--railos-location", help="Location of the RailOS railway.exe file", default="..")
-    _parser.add_argument("--debug", action="store_true", default=False, help="Run in debug mode")
+    _parser.add_argument(
+        "--railos-location",
+        help="Location of the RailOS railway.exe file",
+        default="..",
+    )
+    _parser.add_argument(
+        "--debug", action="store_true", default=False, help="Run in debug mode"
+    )
     _args = _parser.parse_args()
     _railos_loc = _args.railos_location
     if not os.path.exists(_railos_loc):
-        raise AssertionError(f"Launcher failed, could not find Railway Operation Simulator 'railway.exe' at '{_railos_loc}'")
+        raise AssertionError(
+            f"Launcher failed, could not find Railway Operation Simulator 'railway.exe' at '{_railos_loc}'"
+        )
     DiscordBroadcaster(_railos_loc, _args.debug).run()
