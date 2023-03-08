431aa81872ec4da00ade077956fdb418284987a7
Kristian Zarębski <krizar312@gmail.com>
2023-03-08 21:59:45 +0000

    Attempt to fix CI

--- build.sh
+++
@@ -1,2 +0,0 @@
-SITE_PACKAGES=$(python -c 'import sysconfig; print(sysconfig.get_paths()["purelib"])')
-pyinstaller -F --noconsole --paths=$SITE_PACKAGES --icon='img/RailOSLauncher.ico' --clean railos_launcher.py
